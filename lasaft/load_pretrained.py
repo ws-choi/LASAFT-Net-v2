30a490d6f48bad80a21a0668c41e5b88079eecb4
Woosung Choi <ws_choi@korea.ac.kr>
2022-01-11 10:22:18 +0000

    large pretrained

--- lasaft/load_pretrained.py
+++ lasaft/load_pretrained.py
@@ -4,6 +4,30 @@ import hydra
 import torch
 from omegaconf import OmegaConf
 
+
+def get_v2_large_709():
+
+    conf_path = pkg_resources.resource_filename('lasaft', 'pretrained/v2_large/')
+    conf_path = Path(conf_path)
+    ckpt_path = conf_path.joinpath('epoch=709.ckpt')
+
+    with open(conf_path.joinpath('config.yaml')) as f:
+        train_config = OmegaConf.load(f)
+        model_config = train_config['model']
+
+        model = hydra.utils.instantiate(model_config).to('cpu')
+
+        try:
+            ckpt = torch.load(str(ckpt_path), map_location='cpu')
+            model.load_state_dict(ckpt['state_dict'])
+
+            print('checkpoint is loaded '.format(ckpt_path))
+        except FileNotFoundError:
+            print('FileNotFoundError.\n\t {} not exists\ntest mode'.format(ckpt_path))  # issue 10: fault tolerance
+
+    return model
+
+
 def get_mdx_light_v2_699():
     conf_path = pkg_resources.resource_filename('lasaft', 'conf/pretrained/v2_light/')
     conf_path = Path(conf_path)
